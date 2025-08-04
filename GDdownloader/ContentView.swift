2d508de6dcabee62b20745a1a26a6c419ba1317d
MakiforDevelop <makifordevelop@gmail.com>
2025-08-04 13:35:57 +0800

    Initial Commit

---
+++ GDdownloader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GDdownloader
+//
+//  Created by 千葉牧人 on 2025/8/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GDdownloader/GDdownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GDdownloaderApp.swift
+//  GDdownloader
+//
+//  Created by 千葉牧人 on 2025/8/4.
+//
+
+import SwiftUI
+
+@main
+struct GDdownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
